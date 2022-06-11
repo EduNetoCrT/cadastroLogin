fb86afef846818416a9028137d6b06ae4e304ffc
Eduardo Neto <eduardo.js_jp@hotmail.com>
2022-06-11 16:28:00 -0300

    conecção com bando de dados

--- src/app.js
+++ src/app.js
@@ -11,7 +11,16 @@ const app = express();
 app.use(express.json());
 app.use((req, res) => {
     res.json({msg: "Endpoint não encontrado"});
-})
+});
+
+//credencias 
+
+const dbUser = process.env.DB_USER
+const dbPW = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:<${dbPW}>@cluster0.oa1it.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    console.log('Conecção estabeleciada')
+}).catch((err) => console.log(err, "Conecção não estabelecida"))
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT}`);
